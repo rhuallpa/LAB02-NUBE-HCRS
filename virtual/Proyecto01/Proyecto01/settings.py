fa4af7da1cc4ba71c7e04d469457a4bcea761268
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-08 10:03:00 -0500

    test

--- virtual/Proyecto01/Proyecto01/settings.py
+++ virtual/Proyecto01/Proyecto01/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3*1^+kvgner578vta8y7(iyep5pmm34-$+udxr=h13%wzdv@-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
